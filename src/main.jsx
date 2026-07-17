75c56f33036aa60a44b01627408b2457635713ed
Gaurav Kumar <gauravkreth@gmail.com>
2026-07-17 21:41:03 +0530

    Redirect Netlify subdomain

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,17 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// Redirect Netlify subdomain to custom domain
+if (window.location.hostname === 'codezgaurav.netlify.app') {
+  window.location.replace(
+    'https://codezgaurav.in' +
+    window.location.pathname +
+    window.location.search +
+    window.location.hash
+  )
+}
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
